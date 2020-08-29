a60c517e3e1eec25dbf7adce489002c96e6e1577
maribelcuales <maribelcuales@gmail.com>
2020-08-29 17:42:31 +0800

    Created dbConnection.js

---
+++ data/dbConnection.js
@@ -0,0 +1,7 @@
+const knex = require('knex');
+
+const knexfile = require("../knexfile.js"); 
+const environment = process.env.NODE_ENV || "development"; // undefined | production  
+
+module.exports = knex(knexfile[environment]);
+

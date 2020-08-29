886276e05771804d59db524fbaf05efdb41d7dd0
maribelcuales <maribelcuales@gmail.com>
2020-08-29 20:19:50 +0800

    Comments added

--- data/dbConnection.js
+++ data/dbConnection.js
@@ -1,7 +1,21 @@
 const knex = require('knex');
 
 const knexfile = require("../knexfile.js"); 
+
 const environment = process.env.NODE_ENV || "development"; // undefined | production  
 
+
+// const fruitsenv = process.env.FRUITS_ENV || "fruits"; 
+
+// const veggiessenv = process.env.VEGGIES_ENV || "veggies"; 
+
+
+
 module.exports = knex(knexfile[environment]);
+
+// module.exports = {
+//   fruits: knex(knexfile[fruitsenv]),
+//   veggies: knex(knexfile[environment])
+// };
+
 
--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,16 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/produce.db3'
+      // filename: './data/produce.db3',
+      filename: './data/veggies.db3'
     },
     useNullAsDefault: true,  // for SQLite only 
   },
   // npx knex init 
 
   // staging/testing server 
+  // you can delete this part if you are not using it
   staging: {
     client: 'postgresql',
     connection: {
